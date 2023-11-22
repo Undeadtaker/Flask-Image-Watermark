fe70e7c8fce232e05771c1de6bbd78018c88fcc4
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2023-11-22 13:59:00 +0100

    Update main.tf
    Update to the s3 bucket policy which was throwing an error. Now the user can publicly read from the bucket and list all of the images from the bucket.

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -138,15 +138,10 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "AllowPublicRead"
-        Effect = "Allow"
+        Effect   = "Allow"
         Principal = "*"
-        Action = [
-          "s3:GetObject"
-        ]
-        Resource = [
-          "arn:aws:s3:::*"
-        ]
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::s3-flask-bucket/*"
       }
     ]
   })
